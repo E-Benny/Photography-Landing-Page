3f7653703b0d76674a1fe9893f2d12c3478c6696
Jonathan Bennett <82916234+E-Benny@users.noreply.github.com>
2022-06-13 13:18:57 -0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,9 @@
+//Toggle Button
+function toggleMenu() {
+    var navMenu = document.getElementById("mobile-menu-js");
+    if (navMenu.style.height == "0px") {
+      navMenu.style.height = "134px";
+    } else {
+      navMenu.style.height = "0px";
+    }
+  }
